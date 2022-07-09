f58f0f75cc79ff5b03aa19ac61783fe138fccb06
Amy Parker <apark0006@student.cerritos.edu>
2022-07-09 16:27:04 -0700

    Add GPL info, SPDX headers to all source files
    This patch adds GPL info and SPDX headers to all files under the source
    tree. It should be noted that these may be needed in non-source files in
    the future, such as Cargo.toml.
    
    Signed-off-by: Amy Parker <apark0006@student.cerritos.edu>

--- src/bbu/mod.rs
+++ src/bbu/mod.rs
@@ -1,6 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * libxas: Extendable Assembler Library
+ * Copyright (C) 2022 Amy Parker <apark0006@student.cerritos.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA or visit
+ * the GNU Project at <https://gnu.org/licenses>. The GNU General Public
+ * License version 2 is available at, for your convenience,
+ * <https://gnu.org/licenses/old-licenses/gpl-2.0.html>.
+ */
+
 // TODO: specify BBU = "better binutils" or "basic binary units"
 // TODO: clarify vec bits are in exact order, v[0] = binary[0]
 // TODO: move CPU targets into new module
+// TODO: separate project name internally? separate readme?
+//       ties into license declaration perhaps
 
 // TODO: generic argument types (direct, label, memory, register, etc)
 // TODO: general symbol implementation
