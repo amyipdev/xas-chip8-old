2d9af3a490c16870674fb03832bd2ce2da00c7c5
Amy Parker <apark0006@student.cerritos.edu>
2022-08-20 11:38:57 -0700

    [3/3] dead code cleanup
    This patch completes the initial round of dead code cleanup.
    
    Signed-off-by: Amy Parker <apark0006@student.cerritos.edu>

--- src/bbu/outs/mod.rs
+++ src/bbu/outs/mod.rs
@@ -23,34 +23,6 @@
 
 pub mod rawbin;
 
-/*
-// T is the architecture's pointer size
-pub type LabelTree<T> = std::collections::HashMap<String, T>;
-
-pub fn run_output<T: crate::bbu::SymConv>(
-    src: Vec<crate::lexer::LexSection<T>>,
-    dest: &mut Vec<u8>,
-    plat: &crate::platform::Platform,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: opt?
-    match plat.target {
-        crate::platform::PlatformTarget::RawBinary => rawbin::run_output(src, dest, plat),
-    }
-    Ok(())
-}
-
-// TODO: see run_output notes about dynamic pointer sizes
-// NOTE: this is only for things which use automatic linking
-pub fn get_offset(p: &crate::platform::Platform) -> u64 {
-    match (&p.arch, &p.target) {
-        (
-            crate::platform::PlatformArch::ChipEightRaw,
-            crate::platform::PlatformTarget::RawBinary,
-        ) => 0x200,
-    }
-}
- */
-
 pub type LabelTree<T: crate::bbu::SymConv> = std::collections::HashMap<String, T>;
 
 // TODO: fix inherent cloning issues with String
