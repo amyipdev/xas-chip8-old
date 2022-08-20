128856a8bc511ba3e31154810f5557348c0009b8
Amy Parker <apark0006@student.cerritos.edu>
2022-08-20 11:31:15 -0700

    [2/n] bbu: remove parse_unknown_radix_u16
    The last remnants of the old number parsing system are now gone!
    Everything is now running on generics, so this can be removed. Dropping
    this (+ patch 1) reduced the size of BBU *significantly*.
    
    Signed-off-by: Amy Parker <apark0006@student.cerritos.edu>

--- src/bbu/mod.rs
+++ src/bbu/mod.rs
@@ -94,50 +94,6 @@ pub trait ArchMacro {
         Self: Sized;
 }
 
-// FIXME FIXME FIXME FIXME remove this code
-// TODO: optimize, minimize code deduplication
-// TODO: should doubled patterns (0x0x for example) be allowed?
-// trim_start_matches might allow that
-// TODO: proper error handling, return None instead of .unwrap()ing
-// TODO: generally better suite of argument parsing
-pub fn parse_unknown_radix_u16(s: &String) -> Option<u16> {
-    // TODO: is making a copy of the string really necessary?
-    // TODO: would if-elseif be faster?
-    // TODO: when unwrap is handled correctly below this becomes
-    // less necessary
-    match s.len() {
-        0 => return None,
-        // this also might help do away with the last TODO
-        // consider rewriting this whole function later
-        // also, we need a TODO type generic version
-        // something easily replicable across all sizes
-        // more reason to TODO rewrite all of this
-        // this just works as patchwork for now
-        1 => {
-            if s.chars().nth(0) == Some('0') {
-                return Some(0);
-            }
-        }
-        _ => {}
-    }
-    // TODO: dozenal support (XE-based maybe?)
-    // TODO: explore how heavily features should be made
-    Some(match s.chars().nth(0).unwrap() {
-        '0' => match s.chars().nth(1).unwrap() {
-            // lots of duplication here
-            'x' => u16::from_str_radix(s.trim_start_matches("0x"), 16).unwrap(),
-            'b' => u16::from_str_radix(s.trim_start_matches("0b"), 2).unwrap(),
-            // octal is autotrimmed
-            _ => u16::from_str_radix(s, 8).unwrap(),
-        },
-        // more duplication here
-        '-' => u16::from_str_radix(s.trim_start_matches("-"), 10)
-            .unwrap()
-            .wrapping_neg(),
-        _ => u16::from_str_radix(s, 10).unwrap(),
-    })
-}
-
 // TODO NOTE FIXME consider refactoring all of this into parser, so it happens pre-lexing?? maybe??
 
 // TODO NOTE FIXME determine Box vs immutable reference for trait object size fitting, as well as the usage of boxes in general
